c64793d8fa2aef39c2ac173637ffe06962796c0c
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-07-04 17:12:23 +0900

    import Eigen

--- spd/matrix/vector.hpp
+++ spd/matrix/vector.hpp
@@ -1,25 +1,6 @@
 //_/_/_/_/ vector library _/_/_/_//
 #pragma once
+#include <eigen3/Eigen/Core>
 #include <ode/ode.h>
 
-class Vector3
-{
-    double x_;
-    double y_;
-    double z_;
-
-public:
-    Vector3();
-    Vector3(const double x, const double y, const double z);
-    void set(const double x, const double y, const double z);
-    void setIdentity();
-    void setZero();
-    void scale(const double& s);
-    void todReal(dReal* out);
-    Vector3& operator+=(const Vector3& rhs);
-    Vector3& operator-=(const Vector3& rhs);
-friend Vector3 operator+(const Vector3& lhs, const Vector3& rhs);
-friend Vector3 operator-(const Vector3& lhs, const Vector3& rhs);
-friend double dot(const Vector3& lhs, const Vector3& rhs);
-friend Vector3 cross(const Vector3& lhs, const Vector3& rhs);
-};
+void todReal(dReal* out, Eigen::Vector3d vec);
