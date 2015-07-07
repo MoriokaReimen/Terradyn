3d70251663843c4a2f661216aa3972f3caeee9bc
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-07-07 23:07:38 +0900

    mod test

--- terradyn/test/test-wheel_soil.cpp
+++ terradyn/test/test-wheel_soil.cpp
@@ -8,9 +8,15 @@ TEST(WheelSoilTEST, getForce)
   Soil soil;
   Wheel wheel;
   wheel.velocity(0) = 1.0;
-  wheel.velocity(1) = 1.0;
+  wheel.velocity(1) = 2.0;
   wheel.velocity(2) = -1.0;
   WheelSoil wheel_soil(soil, wheel);
-  EXPECT_NEAR(1.0, wheel.velocity(0), 0.01);
+  double buff = wheel_soil.getTheta_m(0.6, toRadian(50));
+  std::cout << toDegree(buff) << std::endl;
+  Eigen::Vector3d force = wheel_soil.getForce(0.6, toRadian(20), toRadian(10));
+  double a[5] ;
+  EXPECT_NEAR(a[0], force(0), 0.01);
+  EXPECT_NEAR(a[1], force(1), 0.01);
+  EXPECT_NEAR(a[2], force(2), 0.01);
 }
 
