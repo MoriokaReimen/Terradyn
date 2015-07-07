3dd482a587fdc82cc74920f06627cb28a55232a4
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-07-07 15:39:34 +0900

    succeed to compile test

--- terradyn/test/test-gsl.cpp
+++ terradyn/test/test-gsl.cpp
@@ -1,25 +1,9 @@
-#include "RotateFunc.hpp"
-
+#include "gsl.hpp"
 #include <gtest/gtest.h>
-using namespace Math3D;
 
-TEST(RotateFuncTest, rotate_func)
+TEST(GSL_Test, integrate)
 {
-  Vector3 x(1.0, 0.0, 0.0);
-  Vector3 y(0.0, 1.0, 0.0);
-  Vector3 z(0.0, 0.0, 1.0);
-  Degree angle(90.0);
-
-  Quaternion quat_a(angle, z);
-  auto ans_a = rotate(quat_a, x);
-  EXPECT_NEAR(0.0, ans_a.x, 0.01);
-  EXPECT_NEAR(1.0, ans_a.y, 0.01);
-  EXPECT_NEAR(0.0, ans_a.z, 0.01);
-
-  Quaternion quat_b(angle, x);
-  auto ans_b = rotate(quat_b, y);
-  EXPECT_NEAR(0.0, ans_b.x, 0.01);
-  EXPECT_NEAR(0.0, ans_b.y, 0.01);
-  EXPECT_NEAR(1.0, ans_b.z, 0.01);
+  auto func = [](double x){return x * x;};
+  EXPECT_NEAR(21.333333, integrate(func, 0, 4), 0.01);
 }
 
